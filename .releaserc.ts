67d7cf10521c9edb995006297beeb94465dbd080
Pierre-MarieMarchio <pierremariemarchio.pro@gmail.com>
2025-04-28 11:30:01 +0200

    feat: added semantic-release-monorepo and lerna

--- .releaserc.ts
+++ .releaserc.ts
@@ -1,7 +1,17 @@
 import type { Options } from 'semantic-release';
 
 const config: Options = {
-  branches: ['semantic-release'],
+  extends: 'semantic-release-monorepo',
+  branches: [{ name: 'semantic-release' }],
+  // tagFormat: '${package.name}-v${version}',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/git',
+  ],
+  dryRun: true,
+  disablePush: true,
 };
 
 export default config;
