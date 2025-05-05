8553a7f48bd21b609afa3c44aa38e58b7f069021
Pierre-MarieMarchio <pierremariemarchio.pro@gmail.com>
2025-05-05 17:24:38 +0200

    fix: fixed versioning script typo

--- .scripts/versioning.js
+++ .scripts/versioning.js
@@ -8,7 +8,7 @@ try {
   switch (currentBranch) {
     case "next":
       console.log("Creating prerelease versions on the next branch");
-      execSync('npm run version:beta-release', {
+      execSync("npm run version:beta-release", {
         stdio: "inherit",
       });
       break;
