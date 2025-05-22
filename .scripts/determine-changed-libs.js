a803b95cb5eec5d243c1a38509870eb8aa7a90d8
Pierre-MarieMarchio <pierremariemarchio.pro@gmail.com>
2025-05-22 19:17:28 +0200

    fix: handle empty lerna changed without error

--- .scripts/determine-changed-libs.js
+++ .scripts/determine-changed-libs.js
@@ -1,32 +1,33 @@
-import { execSync } from "child_process";
+import { spawnSync } from "child_process";
 import { writeFileSync, appendFileSync } from "fs";
 
-try {
-  const output = execSync("npx lerna changed --json", { encoding: "utf-8" });
+const result = spawnSync("npx", ["lerna", "changed", "--json"], {
+  encoding: "utf-8",
+});
 
-  writeFileSync("changed-libs.json", output);
-
-  const libs = JSON.parse(output).map((lib) => lib.name);
+if (result.status === 0 && result.stdout) {
+  const libs = JSON.parse(result.stdout).map((lib) => lib.name);
 
   console.log("Changed libraries:", libs.join(","));
 
+  writeFileSync("changed-libs.json", result.stdout);
   appendFileSync(
     process.env.GITHUB_ENV,
     `changed_libraries=${libs.join(",")}\n`
   );
-
   writeFileSync("changed-libs-output.txt", libs.join(","));
-} catch (err) {
-  const noChanges =
-    err.status === 1 && err.stdout?.includes("No changed packages");
-
-  if (noChanges) {
-    console.log("No changed libraries.");
-    appendFileSync(process.env.GITHUB_ENV, `changed_libraries=\n`);
-    writeFileSync("changed-libs-output.txt", "");
-    process.exit(0);
-  } else {
-    console.error("Error determining changed libraries:", err.message);
-    process.exit(1);
-  }
+} else if (
+  result.status === 1 &&
+  result.stdout.includes("No changed packages")
+) {
+  console.log("No changed libraries.");
+  appendFileSync(process.env.GITHUB_ENV, `changed_libraries=\n`);
+  writeFileSync("changed-libs-output.txt", "");
+  process.exit(0);
+} else {
+  console.error(
+    "Error determining changed libraries:",
+    result.stderr || result.error || "Unknown error"
+  );
+  process.exit(1);
 }
